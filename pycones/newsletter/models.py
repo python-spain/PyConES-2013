4cebb76125516c2e26c43c79d4fac437f4a3dddc
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2013-01-13 10:21:57 +0100

    Added Newsletter and Article models

--- pycones/newsletter/models.py
+++ pycones/newsletter/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Newsletter(models.Model):
+	create_date = models.DateTimeField(editable=False, auto_now_add=True)
+	title = models.CharField(max_length=100)
+	update_date = models.DateTimeField(editable=False, auto_now=True)
+	path = models.CharField(max_length=100)
+	visible = models.BooleanField(default=False)
+
+	def __unicode__(self):
+		return self.title
+
+	@models.permalink
+	def get_absolute_url(self):
+		return ("newsletter",[self.path])
+
+	@property
+	def is_visible(self):
+		return self.visible
+
+		
+
+class Article(models.Model):
+	create_date = models.DateTimeField(editable=False, auto_now_add=True)
+	text = models.TextField()
+	title = models.CharField(max_length=100)
+	update_date = models.DateTimeField(editable=False, auto_now=True)
+	path = models.CharField(max_length=100)
+	publish_date = models.DateTimeField()
+	visible = models.BooleanField(default=False)
+
+	def __unicode__(self):
+		return self.title
+	
+	@models.permalink
+	def get_absolute_url(self):
+		return ("newsletter", [self.path])
+
+	@property
+	def is_visible(self):
+		return self.visible
+
+
