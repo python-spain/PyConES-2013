ad96ba3cae8fe8b51e88ac173d0fc6cd1ebac9ae
Andrey Antukh <niwi@niwi.be>
2013-03-04 19:26:40 +0100

    Fix ugly comments on settings.py

--- pycones/settings.py
+++ pycones/settings.py
@@ -31,14 +31,14 @@ DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 #DATABASES = {
-    #"default": {
-        #"ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        #"NAME": "dev.db",                       # Or path to database file if using sqlite3.
-        #"USER": "",                             # Not used with sqlite3.
-        #"PASSWORD": "",                         # Not used with sqlite3.
-        #"HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
-        #"PORT": "",                             # Set to empty string for default. Not used with sqlite3.
-    #}
+#   "default": {
+#      "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
+#      "NAME": "dev.db",                       # Or path to database file if using sqlite3.
+#      "USER": "",                             # Not used with sqlite3.
+#      "PASSWORD": "",                         # Not used with sqlite3.
+#      "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
+#      "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
+#   }
 #}
 
 # Local time zone for this installation. Choices can be found here:
