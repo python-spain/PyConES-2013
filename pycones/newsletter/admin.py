01b070c3ee55a258efb71cb7ab0efcdae9070162
Yamila Moreno <yamila.ms@gmail.com>
2013-04-10 22:47:43 +0200

    Minor changes to newsletter admin

--- pycones/newsletter/admin.py
+++ pycones/newsletter/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 from pycones.newsletter.models import Newsletter,Article
 
-admin.site.register(Newsletter)
 admin.site.register(Article)
 
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'create_date')
+
+admin.site.register(Newsletter, NewsletterAdmin)
 
