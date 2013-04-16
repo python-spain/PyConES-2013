72805f6f46223dd03c65ca3f90fab22d1a4e948e
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2013-04-16 23:37:36 +0200

    Added Subscription model to administrator

--- pycones/newsletter/admin.py
+++ pycones/newsletter/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from pycones.newsletter.models import Newsletter,Article
+from pycones.newsletter.models import Newsletter,Article,Subscription
 
 admin.site.register(Newsletter)
 admin.site.register(Article)
-
+admin.site.register(Subscription)
 
