5e1c1dff7680af7d91408b4e9267c117a83e982f
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2013-04-16 22:26:23 +0200

    Adding new newsletter views

--- pycones/newsletter/views.py
+++ pycones/newsletter/views.py
@@ -4,17 +4,14 @@ import uuid
 
 from django.shortcuts import render_to_response,redirect
 from django.db import transaction
-from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.template import RequestContext
 from django import http
 
-from pycones.profile.models import Profile
-from .models import Subscription, get_or_create_active_newsletter
-from pycones.profile.models import Profile
+from .models import Subscription
 
 
-def send_welcome_msg(email_user, token, request):
+def send_welcome_msg(email_user, token):
     from django.core.mail import EmailMultiAlternatives
 
     subject = u'¡Bienvenido a PyConES!'
@@ -23,10 +20,8 @@ def send_welcome_msg(email_user, token, request):
     context = {"email": email_user, "token": token}
     context_email = {'email_user' : email_user, 'token' : token}
 
-    text_content = render_to_string("newsletter_welcome_mail.txt", context,
-                                    context_instance=RequestContext(request))
-    html_content = render_to_string("newsletter_welcome_mail.html", context,
-                                    context_instance=RequestContext(request))
+    text_content = render_to_string("newsletter_welcome_mail.txt", context)
+    html_content = render_to_string("newsletter_welcome_mail.html", context)
 
     msg = EmailMultiAlternatives(subject, text_content, from_email, [email_user])
     msg.attach_alternative(html_content, "text/html")
@@ -43,35 +38,25 @@ def suscribe_newsletter(request):
         return redirect('/')
 
     email = request.POST.get('email_user', None)
-    newsletter = get_or_create_active_newsletter()
 
     if not email:
-        context = {'message' : u"Error al recoger el email. Inténtalo de nuevo mas tarde"}
+        context = {'message' : u"Error al recoger el email. Intentalo de nuevo mas tarde"}
         return render_to_response("newsletter/comingsoon_message.html", context,
                                   context_instance=RequestContext(request))
 
-    user_queryset = User.objects.filter(username=email)
+    subscription_queryset = Subscription.objects.filter(user_email=email)
 
     try:
-        user = user_queryset.get()
-    except User.DoesNotExist:
-        user = User(username=email, email=email)
-        user.set_unusable_password()
-        user.save()
-
-        # Create a profile model for new user
-        profile = Profile(user=user, newsletter_token=unicode(uuid.uuid4()))
-        profile.save()
-
-    if Subscription.objects.filter(user=user, newsletter=newsletter).exists():
+        subscription = subscription_queryset.get()
         context = {'message' : u"Se ha producido un error. Quizás ya estes dado de alta."}
-        return render_to_response("newsletter/comingsoon_message.html", context,
-                                  context_instance=RequestContext(request))
+        return render_to_response("newsletter/comingsoon_message.html",
+                                context,
+                                context_instance=RequestContext(request))
+    except Subscription.DoesNotExist:
+        subscription = Subscription(user_email=email, val_token=str(uuid.uuid4()))
+        subscription.save()
 
-    subscriber = Subscription(user=user, newsletter=newsletter)
-    subscriber.save()
-
-    send_welcome_msg(user.email, user.profile.newsletter_token, request)
+    send_welcome_msg(subscription.user_email, subscription.val_token)
 
     context = {'message' : u"Registrado. Muchas gracias"}
     return render_to_response("newsletter/comingsoon_message.html", context,
@@ -88,46 +73,20 @@ def unsuscribe_newsletter(request):
     email = request.GET.get('email', None)
     token = request.GET.get('val_token', None)
 
-    newsletter = get_or_create_active_newsletter()
-
     if not email or not token:
         context = {"message": u"Parametros incorrectos"}
         return render_to_response("newsletter/comingsoon_message.html",
                         context, context_instance=RequestContext(request))
 
-    queryset = User.objects.filter(username=email, profile__newsletter_token=token)
+    queryset = Subscription.objects.filter(user_email=email, val_token=token)
     try:
-        user = queryset.get()
-    except User.DoesNotExist:
+        subscription = queryset.get()
+    except Subscription.DoesNotExist:
         context = {"message": u"Usuario no encontrado."}
     else:
-        subscription_queryset = Subscription.objects.filter(
-                                    newsletter=newsletter, user=user)
-        if subscription_queryset.exists():
-            subscription_queryset.delete()
-            context = {"message": u"Eliminado de la newsletter correctamente"}
-        else:
-            context = {"message": u"Usuario no encontrado."}
+        subscription.delete()
+        context = {"message": u"Eliminado de la newsletter correctamente"}
 
     return render_to_response("newsletter/comingsoon_message.html",
                         context, context_instance=RequestContext(request))
-
-
-def get_latest_newsletter(request):
-    """
-    View to retrieve latest newsletter with their articles
-    """
-    pass
-
-def get_newsletter(request,year_month):
-    """
-    View to retrieve a single newsletter
-    """
-    pass
-
-def send_newsletter(request,year_month):
-    """
-    View to send a newsletter by mail
-    """
-    pass
 
