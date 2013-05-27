c8f0c34a6034f700731b9b39220f6ec9b5240305
Alejandro Gómez <alejandro@dialelo.com>
2013-05-27 21:50:03 +0200

    Alert or success message on newsletter subscription form
    - Meaningful HTTP responses
    - Set content type to JSON
    - Move scripts to the end of the page for faster page load
    
    Missing styles.

--- pycones/web/static/web/js/web.js
+++ pycones/web/static/web/js/web.js
@@ -1,14 +1,13 @@
 ;(function () {
+  var showMessage = function(message, cls) {
+      $('#newsletter-message').html('<p class="' + cls + '">' + message + '</p>')
+                              .show()
+                              .delay(3000)
+                              .fadeOut();
+  };
+
   $('#subscribe-newsletter').click(function(e){
       e.preventDefault();
-
-      var showMessage = function(message, cls) {
-          $('#newsletter-message').html('<p class="' + cls + '">' + message + '</p>')
-                                  .show()
-                                  .delay(3000)
-                                  .fadeOut();
-
-      };
 
       var form = $("#newsletter-form");
 
@@ -20,17 +19,12 @@
            showMessage(data.responseJSON.message, "alert alert-error");
        });
   });
+
   $('#contact-us').click(function(e){
       e.preventDefault();
 
-      var showMessage = function(message, cls) {
-          $('#contact-message').html('<p class="' + cls + '">' + message + '</p>')
-                               .show()
-                               .delay(3000)
-                               .fadeOut();
-          };
-
       var form = $("#contact-us-form");
+
       $.post(form.attr('action'), form.serialize(), null, 'json')
        .done(function(data){
            showMessage(data.message, "alert alert-success");
