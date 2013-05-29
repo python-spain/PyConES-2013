5abd9977dcc53e7d498b792d8f0c4a1df636dcdb
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2013-05-30 00:03:56 +0200

    Unified javascript. Changed style to a.close-button in code_of_conduct aside

--- pycones/web/static/web/js/web.js
+++ pycones/web/static/web/js/web.js
@@ -35,4 +35,34 @@
            showMessage("#contact-us-message", responseJSON.message, "alert alert-error");
        });
    });
+
+  // Sliders
+  $('#patrocinadores-slides').flexslider({
+      controlsContainer: '.patrocinadores-nav',
+      manualControls: '.patrocinadores-nav li'
+   });
+
+  $('#informacion-slides').flexslider({
+      controlsContainer: '.nav-datos',
+      manualControls: '.nav-datos li'
+   });
+  // Fin sliders
+
+  // Code_of_conduct javascript
+  $('#conduct-link').on('click',toggleCodeOfConduct);
+  $('#close-conduct1-link , #close-conduct2-link').on('click',toggleCodeOfConduct);
+
+  function toggleCodeOfConduct(){
+    coc = $('#code_of_conduct');
+    if(coc.css('display') == 'none'){
+        coc.slideDown(1500);
+    }else{
+        coc.slideUp(700);
+    }
+   }
+  // Fin Code_of_conduct javascript
+
 })();
+
+
+
