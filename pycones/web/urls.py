f430763dcb63e757ad060d6bca8a7edf02b8652a
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2013-05-29 23:38:56 +0200

    Deleted url and view to code_of_conduct. Now it's in home.html

--- pycones/web/urls.py
+++ pycones/web/urls.py
@@ -5,7 +5,6 @@ from pycones.web.views import home, contact_us, code_of_conduct
 
 urlpatterns = patterns('pycones.web.urls',
     url(r'^contact-us/$', 'contact_us', name='contact_us'),
-    url(r'^code-of-conduct/$', 'code_of_conduct', name='code_of_conduct'),
     url(r'^$', 'home', name='home'),
 )
 
