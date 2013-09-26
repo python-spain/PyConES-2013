ad65b0a6bad3bde45263d14f54b3ab5a50a70bb0
Yamila Moreno <yamila.ms@gmail.com>
2013-09-26 21:27:52 +0200

    Improving django admin

--- pycones/call4papers/admin.py
+++ pycones/call4papers/admin.py
@@ -6,8 +6,12 @@ from pycones.call4papers.models import Speaker, Talk
 admin.site.register(Speaker)
 
 class TalkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'level', 'selected', 'confirmed')
+    list_display = ('title', 'level', 'talk_speakers', 'selected', 'confirmed')
     list_filter = ('level', 'selected', 'confirmed')
     filter_horizontal = ('speakers',)
+
+    def talk_speakers(self, obj):
+        return ', '.join([sp.name for sp in obj.speakers.all()])
+    talk_speakers.short_description = 'Speakers'
 
 admin.site.register(Talk, TalkAdmin)
